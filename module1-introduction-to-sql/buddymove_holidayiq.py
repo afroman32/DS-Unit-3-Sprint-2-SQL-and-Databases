9092d824d6185c2c29284bd4a329628c4d8e641f
Afroman <makoanoble@gmail.com>
2020-05-25 11:07:14 -0600

    completed stretch goal

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -14,7 +14,7 @@ cursor = connection.cursor()
 df.to_sql('review', con= connection, if_exists= 'replace')
 
 row = "SELECT count(Sports) as rows FROM review"
-print(f'Number of rows: {cursor.execute(row).fetchall()[0][0]}')
+print(f'Number of rows: {cursor.execute(row).fetchall()[0][0]}\n')
 
 reviews = """
         SELECT 
@@ -35,4 +35,23 @@ reviews = """
         """
 over100 = cursor.execute(reviews).fetchall()
 
-print(f'Number of users who reviewed over at least 100 in Nature and Shopping: {over100[0][0]}')
+print(f'Number of users who reviewed over at least 100 in Nature and Shopping: {over100[0][0]}\n')
+
+ave = """
+    SELECT
+        avg(Sports) as Ave_Sport
+        ,avg(Religious) as Ave_Religious
+        ,avg(Nature) as Ave_Nature
+        ,avg(Theatre) as Ave_Theatre
+        ,avg(Shopping) as Ave_Shopping
+        ,avg(Picnic) as Ave_Picnic
+        
+    FROM review
+    """
+result = cursor.execute(ave).fetchall()
+print(f'The average Sports reviews: {result[0][0]}')
+print(f'The average Religious reviews: {result[0][1]}')
+print(f'The average Nature reviews: {result[0][2]}')
+print(f'The average Theatre reviews: {result[0][3]}')
+print(f'The average Shopping reviews: {result[0][4]}')
+print(f'The average Picnic reviews: {result[0][5]}')
